85cdb39d9c335c133c0e99d2ad322a8d7f796b9e
DALERdeveloperWEP <cassidymlbbbbb@gmail.com>
2026-03-22 23:43:10 +0500

    feat: added otp logic

--- apps/bot/bot.py
+++ apps/bot/bot.py
@@ -2,12 +2,14 @@ from telegram import Bot, Update
 from telegram.ext import CommandHandler, Dispatcher
 from django.conf import settings
 
-from .handlers.commands import start
+from .handlers.commands import login, start
 
 bot = Bot(settings.TOKEN)
 dispatcher = Dispatcher(bot, None, workers=4, use_context=True)
 
 dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('login', login))
+dispatcher.add_handler(CommandHandler('help', help))
 
 
 
--- config/settings.py
+++ config/settings.py
@@ -136,4 +136,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 TOKEN = env('BOT_TOKEN')
-WEBHOOK_URL = env('WEBHOOK_URL')
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
